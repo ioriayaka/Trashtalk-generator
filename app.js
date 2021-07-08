05f8a8c2aa9af65907821961e8287b6b0fd12666
ioriayaka <ioriayaka@hotmail.com>
2021-07-08 08:34:24 +0800

    add trash-talk generator function

--- app.js
+++ app.js
@@ -4,17 +4,24 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
 const port = 3000
-
+const list = require('./contents.json')
+const trashTalkGenerator = require('./javascripts.js')
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
-
+// use body-parser
+app.use(express.urlencoded({ extended: true }))
 // setting routes
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { targets: list.job })
+})
+//產生幹話
+app.post('/', (req, res) => {
+  const trashTalk = trashTalkGenerator(req.body.career)
+  res.render('index', { targets: list.job, trashTalk: trashTalk })
 })
 
 // starts the express server and listening for connections.
 app.listen(port, () => {
-  console.log(`Express app listening on port ${port}.`)
+  console.log(`Express app listening on http://localhost:${port}.`)
 })
---
+++ javascripts.js
@@ -0,0 +1,16 @@
+const list = require('./contents.json')
+
+// define sample function to ramdomly return a item in an array
+function sample(array) {
+  const index = Math.floor(Math.random() * array.length)
+  return array[index]
+}
+
+// define trashTalkGenerator function
+function trashTalkGenerator(target) {
+  if (!target) { return '請選一個角色。' }
+  return `身為一個${list.job[target].title}，${sample(list.task[target])}，${sample(list.phrase)}吧！`
+}
+
+// export trashTalkGenerator
+module.exports = trashTalkGenerator
